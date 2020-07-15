c12df4f5986746c67f988b2a036318560f6fbb78
Shaan Bhoi <TechBhoi@gmail.com>
2020-07-15 17:43:39 +0000

    reviewed some for loops and finished random number generator

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import random 
-upper = 89
-lower = 25
+def showMeAllList(mylist):
+	count = 0 
+	for i in mylist:
+		print(i, " is the ", count,"index in the list")
+		count+=1
+	return mylist
 
-randNum = random.randint(lower,upper)
-
-print(randNum)
+##legoSets = ["skullarina","zombiecave","tiaga","pillager outpost","panda nersary "]
+myluckNumbers = [1,2,3,4,5,6,7,8,9]
+showMeAllList(myluckNumbers)
